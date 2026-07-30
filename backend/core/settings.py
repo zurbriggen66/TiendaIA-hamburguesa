6bb31299fd0a7fedfed2fca0e2cdb5a05dfca647
zurbriggen66 <nachozubri15@gmail.com>
2026-07-30 17:28:23 -0300

    nacho sube

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-ww(qv84a)bqrgav_fqesmg(yhod*hy*uhv32=%-spzeq1=#23z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.odettiautomotores.online', 'odettiautomotores.online', 'webapp-3159337.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.odettiautomotores.online', 'odettiautomotores.online', 'webapp-3159337.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
