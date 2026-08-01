15bbd4503f581a0f93a123ff5b7b4e405b4988d8
zurbriggen66 <nachozubri15@gmail.com>
2026-08-01 19:06:46 -0300

    zeta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './VistaCliente/Inicio';
 import Dashboard from './VistaNegocio/Dashboard/Dashboard';
 import DashboardLayout from './VistaNegocio/DashboardLayout';
+import InicioPage from './VistaNegocio/Inicio/Inicio';
 import ProductosPage from './VistaNegocio/Productos/ProductosPage';
 import PedidosPage from './VistaNegocio/Pedidos/PedidosPage';
 import GastosPage from './VistaNegocio/Gastos/GastosPage';
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/admin" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="inicio" element={<InicioPage />} />
           <Route path="productos" element={<ProductosPage />} />
           <Route path="pedidos" element={<PedidosPage />} />
           <Route path="gastos" element={<GastosPage />} />
