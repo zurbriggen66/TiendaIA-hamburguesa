5f72c343a82218b170e77ad631f4b4f146a8275b
zurbriggen66 <nachozubri15@gmail.com>
2026-08-18 12:44:46 -0300

    sfd

--- productos/models.py
+++ productos/models.py
@@ -90,6 +90,19 @@ class Combo(models.Model):
         return self.nombre
 
 
+class Presentacion(models.Model):
+    producto = models.ForeignKey(Producto, related_name='presentaciones', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    orden = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['orden', 'precio']
+
+    def __str__(self):
+        return f'{self.producto.nombre} - {self.nombre}'
+
+
 class ComboItem(models.Model):
     combo = models.ForeignKey(Combo, related_name='items', on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.PROTECT, related_name='combo_items')
